52c0d37873f1b24088b48223669ec9a36aa44c5e
ktsmproger <ktsmadm6@gmail.comktsmadm6@gmail.com>
2020-11-24 15:17:40 +0200

    Контроллер на виртуальных дутах готов

--- TestAsync/Dutyara.cs
+++ TestAsync/Dutyara.cs
@@ -15,6 +15,22 @@ namespace TestAsync
         public static int current_speed;
         static public bool need_a_stop = false;
 
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public struct MessageContent
+        {
+            public string id;
+            public string fuel;
+            public string water;
+            public string temp;
+        }
+
         public Dutyara(int id, int speed = 9600)
         {
             this.id = id;
@@ -26,7 +42,7 @@ namespace TestAsync
             var ans = "N0=+210=01345.27=00632.55=094";
             //string answer = "Ответ от " + this.id + ": " + ans;
             string answer = this.id + ans;
-            int timeout = rnd.Next(1000, 7500);
+            int timeout = rnd.Next(1000, 5000);
             if (timeout <= 5000)
             {
                 return answer;
--- TestAsync/Program.cs
+++ TestAsync/Program.cs
@@ -18,7 +18,10 @@ namespace TestAsync
         static string dut_data = "";
         // Дремя, которое даётся уту на то чтобы дать ответ
         static int time_to_dut_read = 3000;
-        int
+        static int? message_status = null;
+        const int MSG_SUCCESS = 1, MSG_FAIL = 0, MSG_DROP = -1;
+        const string FAIL_VALUE = "65536", DROP_VALUE = "65533";
+        public static Dutyara.MessageContent message_content = new Dutyara.MessageContent();
 
 
         static void Main(string[] args)
@@ -26,7 +29,7 @@ namespace TestAsync
             dut_list.Add(new Dutyara(1, 44));
             dut_list.Add(new Dutyara(2, 44));
             DutControl();
-            while(true)
+            //while (true)
             {
                 Console.WriteLine(Console.ReadLine());
             }
@@ -50,24 +53,45 @@ namespace TestAsync
 
                     //if (!Dutyara.opened)
                     {
+                        //dut_data = "44N0=+210=01345.27=00632.55=094";
                         if (dut_data != "")
                         {
-                            CheckData();
+                            CheckData(dut_data);
                         }
+                        // Так должно быть лучше, но нужно проверит, а на это нет времени:
+                        else
                         if (sw.ElapsedMilliseconds > time_to_dut_read)
                         {
-                            Dutyara.need_a_stop = true;
-                            //Thread.Sleep(50);
-                            Dutyara.opened = true;
-                            if (dut_data == "")
-                            {
-                                string ans = (dut_selected + 1) + "N0=65536=65536=65536=094";
-                                Console.WriteLine("Error from " + (dut_selected + 1) + ": " + ans);
-                            }
-                            else
-                                Console.WriteLine("Dut[" + (dut_selected + 1) + "] return answer: " + dut_data);
-                            dut_data = "";
-                            dut_selected = (dut_selected + 1) % dut_list.Count();
+                            message_status = MSG_FAIL;  
+                        }
+
+                        switch (message_status)
+                        {
+                            case MSG_FAIL:
+                                message_content.id = dut_list[dut_selected].Id.ToString();
+                                message_content.water = FAIL_VALUE;
+                                message_content.fuel = FAIL_VALUE;
+                                message_content.temp = FAIL_VALUE;
+                                //dut_data = "44N0=65536=65536=65536=094";
+                                break;
+                            case MSG_DROP:
+                                message_content.id = dut_list[dut_selected].Id.ToString();
+                                message_content.water = DROP_VALUE;
+                                message_content.fuel = DROP_VALUE;
+                                message_content.temp = DROP_VALUE;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        if (message_status != null)
+                        {
+                            if (message_status != MSG_SUCCESS)
+                            dut_data = message_content.id + "N0=" + message_content.temp + "=" +
+                                            message_content.fuel + "=" + message_content.water + "=094"; //"44N0=65536=65536=65536=094";
+                            Console.WriteLine(dut_data);
+
+                            GoToNextDut();
                         }
                     }
 
@@ -86,9 +110,53 @@ namespace TestAsync
         // Проверка полученных данных от ДУТа
         // в случае, если данные дошли в целосности - отправляет их получателю
         // в случае если данные пришли в повреждённом виде - отправляем получателю соответствующий код ошибки
-        private static int CheckData()
+        private static void CheckData(string input_text)
         {
-            throw new NotImplementedException();
+            string[] dut_data_arr = input_text.Split('=');
+            int arr_len = dut_data_arr.Length;
+            if (arr_len != 5)
+            {
+                message_status = MSG_DROP;
+            }
+            else
+            {
+                // Проверяем является ли айдишник числом
+                string dut_id = dut_data_arr[0].Substring(0, dut_data_arr[0].Length - 2);
+                float i = 0;
+                var bb = float.TryParse(dut_id, out i);
+                if (!bb)
+                {
+                    message_status = MSG_DROP; return;
+                }
+                if (dut_data_arr[1][0] != '+' && dut_data_arr[1][0] != '-')
+                {
+                    message_status = MSG_DROP; return;
+                }
+                bb = float.TryParse(dut_data_arr[2].Replace(".", ","), out i);
+                if (!bb)
+                {
+                    message_status = MSG_DROP; return;
+                }
+                bb = float.TryParse(dut_data_arr[3].Replace(".", ","), out i);
+                if (!bb)
+                {
+                    message_status = MSG_DROP; return;
+                }
+                // ХЗ что хз зачем, но вдроуг пригодится
+                //bb = float.TryParse(dut_data_arr[4].Replace(".", ","), out i);
+                //if (!bb)
+                //{
+                //    message_status = MSG_DROP; return;
+                //}
+
+                message_status = MSG_SUCCESS;
+                message_content.id = dut_id;
+                message_content.fuel = dut_data_arr[2];
+                message_content.water = dut_data_arr[3];
+                message_content.temp = dut_data_arr[1];
+
+            }
+            return;
         }
 
         // Перейти к опросу следующего ДУТа 
